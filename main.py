3487beac7d6dc3fad5cf59a111cbfb301171e058
piotr sobieraj <96738453+piotr-sobieraj@users.noreply.github.com>
2023-03-20 18:38:39 +0000

    Wydzielenie klasy Board

--- main.py
+++ main.py
@@ -1,42 +1,35 @@
-from time import sleep
 import pygame
-
 import constants as const
 from tank import Tank
+from board import Board
 
-
-pygame.init()
-window = pygame.display.set_mode((const.windowWidth, const.windowHeight))
-pygame.display.set_caption("Tanks battle")
-
-# Create tanks
-redTank = Tank(const.red)
-whiteTank = Tank(const.white)
-
-# Main game loop
-while True:
-    # Slows down the animation. 
-    sleep(0.5)
+if __name__ == "__main__":
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit
+    # Create tanks
+    redTank = Tank(const.red)
+    whiteTank = Tank(const.white)
     
-    # Tactics of the red tank - should be put in the fight method.
-    redTank.rotate(5)
-    redTank.move(10)
-
-    # Tactics of the white tank - it should be always put in the fight method. 
-    whiteTank.fight()
-
-    # Detection of a hit.
-    print(Tank.detectHit(redTank, whiteTank)) #shot fired by the white tank
-    print(Tank.detectHit(whiteTank, redTank)) #shot fired by the red tank
-
+    # Board with two tanks on it
+    board = Board(redTank, whiteTank)        
     
-
-    #Drawing / refreshing screen
-    window.fill(const.green)
-    redTank.draw(window)
-    whiteTank.draw(window)
-    pygame.display.update()    
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit
+        
+        # Tactics of the red tank - should be put in the fight method.
+        redTank.rotate(-15)
+        redTank.move(10)
+        
+        # Tactics of the white tank - it should be always put in the fight method. 
+        whiteTank.fight()
+    
+        # Detection of a hit.
+        if (Tank.detectHit(redTank, whiteTank)):
+            board.setCaption("White hit the red!")
+        elif Tank.detectHit(whiteTank, redTank):
+            board.setCaption("Red hit the white!")
+        
+        board.redraw()    
+        board.slowDown()
