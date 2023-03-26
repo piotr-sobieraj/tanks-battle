18e8fbd0ab99a17d416a97005675c337ea0633d0
piotr sobieraj <96738453+piotr-sobieraj@users.noreply.github.com>
2023-03-26 14:31:32 +0000

    rozkminka kąta - wip

--- board.py
+++ board.py
@@ -1,17 +1,17 @@
 from time import sleep
+from math import atan2, degrees
 import pygame
 import constants as const
 
 
 class Board:
-    def __init__(self, *tanks):
-        self.tank1, self.tank2 = tanks      
+    def __init__(self, *tanks): 
+        self.tank1, self.tank2 = tanks # tank1 = red, tank2 = white
         self.points = {x: 0 for x in tanks} | {"total": 0}         
        
         pygame.init()
         self.setWindowMode()
-        self.setCaption("Welcome to Tanks Battle!")
-        
+        self.setCaption("Welcome to Tanks Battle!")        
     
     def setWindowMode(self):
         self.window = pygame.display.set_mode((const.windowWidth, const.windowHeight))
@@ -58,4 +58,32 @@ class Board:
         d = self.calcDistanceBetweenTanks()
         self.tank1.distanceToOpponent = d
         self.tank2.distanceToOpponent = d
+
+    def updateAngleInfo(self):
+        a = self.calculateAngleDiff()
+        
+        if a is not None:
+            self.tank1.angleToOpponent = -a
+            self.tank2.angleToOpponent = a
+
+    def calculateAngleDiff(self):
+        if self.tank1 is None or self.tank2 is None: return 
+        
+        # Get position and angle of second tank
+        x2, y2 = self.tank2.x, self.tank2.y
+        angle2 = self.tank1.angle
+    
+        # Calculate angle to second tank from current tank
+        dx, dy = x2 - self.tank2.x, y2 - self.tank2.y
+        angleTo = degrees(atan2(dy, dx))
+    
+        # Calculate difference in angle between current tank and second tank
+        angleDiff = angleTo - self.tank2.angle
+        angleDiff = angleDiff % 360
+        angleDiff = (angleDiff + 180) % 360 - 180
+    
+        # Add the rotation of the second tank to the angle difference
+        angleDiff += angle2
+        return angleDiff
+        print(angleDiff, angleDiff % 360)
         
--- main.py
+++ main.py
@@ -11,30 +11,47 @@ if __name__ == "__main__":
     
     # Board with two tanks on it
     board = Board(redTank, whiteTank)        
-    
-    # Main game loop
-    while not board.isGameOver():
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit
 
-        board.updateDistanceInfo()
-        # Tactics of the red tank - should be put in the fight method.
-        redTank.rotate(45)
-        redTank.move(20)
+    redTank.rotate(30)
+    redTank.rotate(30)
+    redTank.rotate(30)
+    redTank.rotate(30)
+    redTank.rotate(30)
+    redTank.rotate(30)
+    redTank.rotate(30)
+    
+    
+    
+    board.updateAngleInfo()
+    print(f"{redTank.angleToOpponent=}, {whiteTank.angleToOpponent=}")
+    board.redraw()
+
+
+    # # Main game loop
+    # while not board.isGameOver():
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             pygame.quit
+
+    #     board.updateDistanceInfo()
+    #     board.updateAngleInfo()
         
-        # board.distanceBetweenTanks()
-        # Tactics of the white tank 
-        whiteTank.fight()
-        # board.distanceBetweenTanks()
+    #     # Tactics of the red tank - should be put in the fight method.
+    #     redTank.rotate(20)
+    #     redTank.move(20)
+        
+    #     board.updateDistanceInfo()
+    #     board.updateAngleInfo()
+        
+    #     whiteTank.fight()
     
-        # Detection of a hit.
-        if (Tank.detectHit(redTank, whiteTank)):
-            board.addPointsTo(redTank)
-        elif Tank.detectHit(whiteTank, redTank):
-            board.addPointsTo(whiteTank)
+    #     # Detection of a hit.
+    #     if (Tank.detectHit(redTank, whiteTank)):
+    #         board.addPointsTo(redTank)
+    #     elif Tank.detectHit(whiteTank, redTank):
+    #         board.addPointsTo(whiteTank)
 
-        # board.calcDistanceBetweenTanks()
-        # print(board.distanceBetweenTanks)
-        board.redraw()    
-        board.slowDown()
+        
+        
+    #     board.redraw()    
+    #     board.slowDown()
